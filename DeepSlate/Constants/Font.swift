67291cc837d5ef7dbc09bc9462a9826bab3ff2f0
Kwame <kwameagyenimboateng64@gmail.com>
2022-11-13 17:40:19 +0000

    font types

---
+++ DeepSlate/Constants/Font.swift
@@ -0,0 +1,18 @@
+//
+//  Font.swift
+//  DeepSlate
+//
+//  Created by Kwame Agyenim - Boateng on 12/11/2022.
+//
+
+import Foundation
+
+
+enum Font: String {
+    case bold = "EuclidCircularB-Bold"
+    case semibold = "EuclidCircularB-SemiBold"
+    case medium = "EuclidCircularB-Medium"
+    case regular = "EuclidCircularB-Regular"
+    case light = "EuclidCircularB-Light"
+    
+}
