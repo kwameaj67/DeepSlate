97e7f2f1a041f3ad8ccef0493e05e20379dc861f
Kwame <kwameagyenimboateng64@gmail.com>
2022-11-13 17:43:17 +0000

    extra screens for tabs

---
+++ DeepSlate/Controllers/DiscoverVC.swift
@@ -0,0 +1,30 @@
+//
+//  DiscoverVC.swift
+//  DeepSlate
+//
+//  Created by Kwame Agyenim - Boateng on 12/11/2022.
+//
+
+import UIKit
+
+class DiscoverVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupContraints()
+        view.backgroundColor = UIColor(red: 0.97, green: 0.97, blue: 0.98, alpha: 1.00)
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+
+   // MARK: Properties -
+ 
+    func setupViews(){
+        
+    }
+    func setupContraints(){
+        
+    }
+
+}
---
+++ DeepSlate/Controllers/ProfileVC.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileVC.swift
+//  DeepSlate
+//
+//  Created by Kwame Agyenim - Boateng on 12/11/2022.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupContraints()
+        view.backgroundColor = UIColor(red: 0.97, green: 0.97, blue: 0.98, alpha: 1.00)
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+
+   // MARK: Properties -
+  
+    func setupViews(){
+        
+    }
+    func setupContraints(){
+        
+    }
+
+}
